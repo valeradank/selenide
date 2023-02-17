0517a2cf2eeab5b9f6ef0dc3f311ed72806c3515
valo <valorapaulinio@gmail.com>
2023-02-17 19:06:22 +0300

    add driver manager

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,6 +1,12 @@
 import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 import java.time.Duration;
@@ -10,6 +16,22 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideTest {
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
 @Test
     void cardDeliveryTest(){
     Configuration.holdBrowserOpen = true;
