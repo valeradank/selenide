28c4619adff6a5bcc87638af4f6a8ea1bcd1ab31
valo <valorapaulinio@gmail.com>
2023-02-17 19:15:28 +0300

    reformate code

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -31,25 +31,29 @@ public class SelenideTest {
     @AfterEach
     void teardown() {
         driver.quit();
+
+
     }
-@Test
-    void cardDeliveryTest(){
-    Configuration.holdBrowserOpen = true;
-    open("http://localhost:9999/");
-    $("input[type='text']").setValue("Астрахань");
-    $("input[type='tel']").sendKeys(Keys.BACK_SPACE);
-    $("input[type='tel']").sendKeys(Keys.BACK_SPACE);
-    $("input[type='tel']").sendKeys(Keys.BACK_SPACE);
-    $("input[type='tel']").sendKeys(Keys.BACK_SPACE);
-    $("input[type='tel']").sendKeys(Keys.BACK_SPACE);
-    $("input[type='tel']").sendKeys(Keys.BACK_SPACE);
-    $("input[type='tel']").sendKeys(Keys.BACK_SPACE);
-    $("input[type='tel']").sendKeys(Keys.BACK_SPACE);
-    $("input[type='tel']").setValue("22.02.2023");
-    $("input[name='name']").setValue("Валерий Данковцев");
-    $("input[name='phone']").setValue("+79295964548");
-    $("[data-test-id='agreement']").click();
-    $x("//span[text()='Забронировать']").click();
-    $("div[data-test-id='notification']").should(appear, Duration.ofSeconds(15));
-}
+
+
+    @Test
+    void cardDeliveryTest() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        $("input[type='text']").setValue("Астрахань");
+        $("input[type='tel']").sendKeys(Keys.BACK_SPACE);
+        $("input[type='tel']").sendKeys(Keys.BACK_SPACE);
+        $("input[type='tel']").sendKeys(Keys.BACK_SPACE);
+        $("input[type='tel']").sendKeys(Keys.BACK_SPACE);
+        $("input[type='tel']").sendKeys(Keys.BACK_SPACE);
+        $("input[type='tel']").sendKeys(Keys.BACK_SPACE);
+        $("input[type='tel']").sendKeys(Keys.BACK_SPACE);
+        $("input[type='tel']").sendKeys(Keys.BACK_SPACE);
+        $("input[type='tel']").setValue("22.02.2023");
+        $("input[name='name']").setValue("Валерий Данковцев");
+        $("input[name='phone']").setValue("+79295964548");
+        $("[data-test-id='agreement']").click();
+        $x("//span[text()='Забронировать']").click();
+        $("div[data-test-id='notification']").should(appear, Duration.ofSeconds(15));
+    }
 }
